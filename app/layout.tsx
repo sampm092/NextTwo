f4bc2e0b2a3af8b29978bb126274c5f06dc7d877
SamPM092 <sammich4294@gmail.com>
2026-05-09 13:04:08 +0700

    learn: importing fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import '@/app/ui/global.css'
+import '@/app/ui/global.css';
+import { interFont } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+       {/* antialiased class smooths out the font */}
+      <body className={`${interFont.className} antialiased`}>{children}</body>
     </html>
   );
 }
