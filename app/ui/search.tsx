4e4340df720fb28a717b9aaad95f3c2b231f4ec0
SamPM092 <sammich4294@gmail.com>
2026-05-15 14:36:09 +0700

    comment(pagination.tsx): handleSearch

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,16 +11,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   // This function will wrap the contents of handleSearch, and only run the code after a specific time once the user has stopped typing (300ms).
   const handleSearch = useDebouncedCallback((term) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page','1');
+    const params = new URLSearchParams(searchParams); //take current URL parameter into params so we can edit it
+    params.set('page','1'); // set page to the first page every time user search new word
     if (term) {
-      params.set("query", term);
+      params.set("query", term); // if theres any search 'words', add to URL: ?query='words'
     } else {
-      params.delete("query");
+      params.delete("query"); // if input empty, erase parameter from url
     }
-    replace(`${pathname}?${params.toString()}`);
+    replace(`${pathname}?${params.toString()}`); // rewrite url with pathname (/dashboard/invoices) and parameters (query='words'&page=1))
     console.log(`Searching... ${term}`);
-  }, 300);
+  }, 300); //wait 300ms, then continue search process
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -30,10 +30,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
+        onChange={(e) => { // make sure input detection done real time word per word
+          handleSearch(e.target.value); // take the latest input
         }}
-        defaultValue={searchParams.get("query")?.toString()}
+        defaultValue={searchParams.get("query")?.toString()} // take parameter from URL as a current input (if ?query=Book, then inside search input there'll be 'Book')
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
